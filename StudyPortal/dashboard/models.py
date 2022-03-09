d8b604e4e8b0f93b643aaad66d32ecfb8331bef8
MukulJoshi6312 <80614782+MukulJoshi6312@users.noreply.github.com>
2022-03-09 19:57:54 +0530

    StudyPortal
    online Study Portal

---
+++ StudyPortal/dashboard/models.py
@@ -0,0 +1,42 @@
+from math import fabs
+from pydoc import describe
+from tkinter import CASCADE
+from turtle import title
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Notes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tilt = models.CharField(max_length=200)
+    description = models.TextField()
+    
+    def __str__(self):
+        return   self.user , self.tilt
+    
+    class Meta:
+        verbose_name = 'Notes'
+        verbose_name_plural = 'Notes'
+    
+    
+
+class Homework(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    subject = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    due = models.DateTimeField()
+    is_finished = models.BooleanField(default=False)  
+    
+     
+    def __str__(self):
+        return self.title
+    
+    
+class Todo(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    is_finished = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
